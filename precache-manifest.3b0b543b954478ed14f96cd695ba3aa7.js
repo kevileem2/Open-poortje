d7cc5269d85ac03914de831d5acce7dedb52b8f8
kevileem2 <kevileem2@student.arteveldehs.be>
2020-03-26 16:47:03 +0100

    Updates

---
+++ precache-manifest.3b0b543b954478ed14f96cd695ba3aa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8b94ecf8425368429939e142eadeb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7db536814bf35fb4e1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e68bd5cccce45cf963c7",
+    "url": "/static/js/2.41c13158.chunk.js"
+  },
+  {
+    "revision": "dc7db536814bf35fb4e1",
+    "url": "/static/js/main.0e316dd9.chunk.js"
+  },
+  {
+    "revision": "9d61b03d4f6127aa72cb",
+    "url": "/static/js/runtime~main.5c085fde.js"
+  }
+]);

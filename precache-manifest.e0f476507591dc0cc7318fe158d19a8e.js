420a0793e905f6d2eb809e1ede99cd0055eca173
kevileem2 <kevileem2@student.arteveldehs.be>
2020-03-26 17:19:20 +0100

    Updates

---
+++ precache-manifest.e0f476507591dc0cc7318fe158d19a8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fb6fc557f177b7708e46a436742018",
+    "url": "/Open-poortje/index.html"
+  },
+  {
+    "revision": "b9562f0b913269835498",
+    "url": "/Open-poortje/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0dfa2fd91ba50cf7e42c",
+    "url": "/Open-poortje/static/js/2.87b58d73.chunk.js"
+  },
+  {
+    "revision": "b9562f0b913269835498",
+    "url": "/Open-poortje/static/js/main.275bd87e.chunk.js"
+  },
+  {
+    "revision": "91b208d1209bd0b22f14",
+    "url": "/Open-poortje/static/js/runtime~main.29f84a89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b0b543b954478ed14f96cd695ba3aa7.js"
+  "/Open-poortje/precache-manifest.e0f476507591dc0cc7318fe158d19a8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Open-poortje/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
